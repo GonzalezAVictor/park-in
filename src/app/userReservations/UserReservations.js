c7dc101898d9c4b8510a69546a053859ecb75134
Raul Canul <eduardoct120@gmail.com>
2018-05-19 23:53:08 -0500

    UserReservations component connected to firebase

--- src/app/userReservations/UserReservations.js
+++ src/app/userReservations/UserReservations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase';
 
 //Components
 import UserReservationsCard from '../components/userReservationsCard/UserReservationsCard';
@@ -8,16 +9,68 @@ require('./UserReservations.scss');
 
 export default class UserReservations extends React.Component {
 
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentWillMount = () => {
+    const user = "Mobile";
+    const isLogin = true;
+
+    if (isLogin) {
+      firebase.database().ref(`users/borrowers/${user}/history`).once('value')
+        .then( snapshot => {
+          const response = snapshot.val();
+          const reservations = [];
+          
+          if (response) {
+            response.map(item => item !==null ? reservations.push(item) : ()=>{}  )
+            this.setState({reservations});
+          } else {
+            this.setState({reservations: []});
+          }
+        } )
+    } else {
+      // REDIRECT TO LOGIN
+      
+    }
+    
+  }
+
+  getReservations = () => {
+    const {reservations} = this.state
+    const reservationsCard = []
+
+    reservations.map( reservation => 
+      reservationsCard.push((
+        <UserReservationsCard 
+          key={reservation.id}
+          place={reservation.place}
+          address={reservation.address}
+          entranceHour={`${reservation.entranceHour}pm`}
+          date={reservation.date}
+          price={`$${reservation.price}/hr`}
+        />
+      )));
+    
+    return reservationsCard
+  }
+
   render() {
-    let reservationsNumber = 2;
+    const {reservations} = this.state;
+    let reservationsNumber = reservations.length;
+    const reservationsCards = this.getReservations()
 
     return (
       <div className="user-reservations">
-        <label id="reservations-main-text" class="mt-5">Tus reservaciones <label id="reservations-number">({reservationsNumber})</label></label>
+        <label id="reservations-main-text" className="mt-5">Tus reservaciones <label id="reservations-number">({reservationsNumber})</label></label>
         
         <div id="cards-section">
-          <UserReservationsCard place="Estacionamiento Santa Anita" address="Calle 63 #238 Mérida Centro 09300" entranceHour="5:00pm" date="15 Mayo" price="$25/hr" />
-          <UserReservationsCard place="Estacionamiento San Ramon" address="Calle 63 #238 Mérida Centro 09300" entranceHour="5:00pm" date="15 Mayo" price="$35/hr" />
+          {reservationsCards}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss'
 import App from './App';
-import firebase from 'firebase'
+import firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 import "bootstrap/scss/bootstrap.scss";
 
