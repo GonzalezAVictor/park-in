13f80bc58a47ca0d527a8ea9d96dcd74682f6668
Raul Canul <eduardoct120@gmail.com>
2018-05-20 21:13:16 -0500

    Firebase implemented on RegisterParkLot component

--- src/app/associateRegisterParkLot/RegisterParkLot.js
+++ src/app/associateRegisterParkLot/RegisterParkLot.js
@@ -1,24 +1,83 @@
 import React from 'react';
+import firebase from 'firebase';
 import './RegisterParkLot.scss';
 
 export default class RegisterParkLot extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      place: '',
+      address: '',
+      spotsNumber: '',
+      startHour: '',
+      finishHour: ''
+    }
+  }
+
+  componentWillMount = () => {
+    this.loadData();
+  }
+
+  addSpot = () => {
+    let {spotsNumber, place} = this.state;
+    spotsNumber++;
+
+    this.setState({spotsNumber}, () => {
+      let updates = {}
+      updates['/spotsNumber'] = spotsNumber;
+
+      firebase.database().ref(`parking_lots/${place}`).update(updates);
+    })
+  }
+
+  removeSpot = () => {
+    let {spotsNumber, place} = this.state;
+    spotsNumber--;
+
+    this.setState({spotsNumber}, ()=>{
+      let updates = {}
+      updates['/spotsNumber'] = spotsNumber;
+
+      firebase.database().ref(`parking_lots/${place}`).update(updates);
+    })
+  }
+
+  loadData = () => {
+    const user = localStorage.getItem("user");
+
+    firebase.database().ref(`users/owners/${user}/lots`).once( 'value', snapshot => {
+      console.log(snapshot.val());
+      const parkinLot = snapshot.val()[0]
+
+      firebase.database().ref(`parking_lots/${parkinLot}`).once('value', snap => {
+        console.log(snap.val());
+
+        const {place, address, spotsNumber, startHour, finishHour} = snap.val();
+        this.setState({place, address, spotsNumber, startHour, finishHour});
+      })
+    } )
+  }
+
   render() {
+    const {place, address, spotsNumber, startHour, finishHour} = this.state
+
     return (
       <div className="register-park-lot">
         <div className="lugares-registrados">Tus lugares registrados</div>
         <div className="park-lot-info-container">
-          <h3>Estacionamiento <br/> Santa Anita <br/>
-          <span className="address">Calle 62 #238 Mérida Centro</span></h3>
+          <h3>{place}<br/>
+          <span className="address">{address}</span></h3>
           
           <div className="pl-registered">
-            <i class="fas fa-plus"></i>
-            <span>8</span>
-            <i class="fas fa-plus"></i>
+            <i class="fas fa-minus" onClick={this.removeSpot}></i>
+            <span>{spotsNumber}</span>
+            <i class="fas fa-plus" onClick={this.addSpot}></i>
           </div>
           <div className="schedule">
             <h5>Horario</h5>
-            9am - 12pm
+            {`${startHour} - ${finishHour}`}
           </div>
           <div className="amenidades">
             Amenidades de tu <br/>
